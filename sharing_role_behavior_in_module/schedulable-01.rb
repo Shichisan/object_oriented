4f6a4c9f0a59a07b72641b399f57f100b2200b17
Shichisan <taiga.shichisan@gmail.com>
2020-04-25 18:53:33 +0900

    :sparkles: add sharing_role_behavior_in_module part

---
+++ sharing_role_behavior_in_module/schedulable-01.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module Schedulable
+  attr_writer :schedule
+
+  def schedule
+    @schedule ||= ::Schedule.new
+  end
+
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+
+  # 必要に応じてインクルードする側で置き換える
+  def lead_days
+    0
+  end
+end
+
+class Bicycle
+  include Schedulable
+
+  attr_reader :schedule, :size, :chain, :tire_size
+
+  def lead_days
+    1
+  end
+
+  def initialize(args={})
+    @schedule = args[:schedule] || Schedule.new
+  end
+
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+end
+
+class Vehicle
+  include Schedulable
+
+  def lead_days
+    3
+  end
+end
+
+class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+end
+
+require 'date'
+starting = Date.parse("2015/09/04")
+ending = Date.parse("2015/09/10")
+
+b = Bicycle.new
+b.schedulable?(starting, ending)
+
+v = Vehicle.new
+v.schedulable?(starting, ending)
+
+m = Mechanic.new
+m.schedulable?(starting, ending)
+
